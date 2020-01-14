be3f2b4abe9bce3a8207e6e269adb342332c6527
Prashanth Ashok Ramkumar <Prashanth.Ashok.Ramkumar@morganstanley.com>
2020-01-14 22:52:00 +0000

    Adding bootstrap

---
+++ src/app/app-bootstrap.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertModule } from 'ngx-bootstrap';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AlertModule.forRoot()
+  ],
+  exports: [AlertModule]
+})
+export class AppBootstrapModule { }
