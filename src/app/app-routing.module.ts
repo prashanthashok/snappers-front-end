991e30a254ea721f4bd81877f684332273ef2e15
Prashanth Ashok Ramkumar <augmented_realtors@outlook.com>
2020-01-17 13:57:21 -0500

    Loading squirrels...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
-import { LoaderComponent } from './loader/loader.component';
 
 const routes: Routes = [
     { path: 'uploadimage', component: ImageUploadComponent },
