c0e47a20a665b6993ccfb6105d83d7ef6042d664
Prashanth Ashok Ramkumar <Prashanth.Ashok.Ramkumar@morganstanley.com>
2020-01-24 17:09:18 +0000

    Changing Image Upload API URL

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,4 @@
 //export const UPLOADIMAGEAPI = 'http://localhost:5000/api/images/upload';
-export const API_UPLOADTOBLOB = 'http://localhost:56057/api/images/upload';
+//export const API_UPLOADTOBLOB = 'http://localhost:56057/api/images/upload';
+export const API_BLOB = 'https://az-snappers-image-upload.azurewebsites.net'
+export const API_UPLOADTOBLOB = `${API_BLOB}/api/images/upload`;
