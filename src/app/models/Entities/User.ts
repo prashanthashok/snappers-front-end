712f45b6300546e5470b00dd1cca4c647d9a3f76
Prashanth Ashok Ramkumar <Prashanth.Ashok.Ramkumar@morganstanley.com>
2020-01-24 20:22:35 +0000

    User model

--- src/app/models/Entities/User.ts
+++ src/app/models/Entities/User.ts
@@ -1,8 +1,7 @@
 export class User {
-    id: string;
     username: string;
-    password: string;
     firstName: string;
     lastName: string;
-    creditScore: number;
+    emailAddress: string;
+    phone: string;
 }
